6513af20c3fa1d7f2bea8e800514f1c05a99f650
Gaby Mendez <gmendez90@gmail.com>
2020-06-10 11:19:38 -0400

    Finish Iteration 1

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -6,6 +6,8 @@ require './lib/vendor'
 class VendorTest < Minitest::Test
   def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   def test_it_exists
@@ -13,7 +15,29 @@ class VendorTest < Minitest::Test
   end
 
   def test_it_has_a_name
-    assert_equal 'Rocky Mountain Fresh', @vendor.name 
+    assert_equal 'Rocky Mountain Fresh', @vendor.name
   end
 
+  def test_starts_with_no_inventory
+    assert_equal Hash.new, @vendor.inventory
+  end
+
+  def test_can_check_stock
+    assert_equal 0, @vendor.check_stock(@item1)
+  end
+
+  def test_can_stock_items
+    @vendor.stock(@item1, 30)
+
+    assert_equal ({@item1 => 30}), @vendor.inventory
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+
+    assert_equal ({@item1 => 55, @item2 => 12}), @vendor.inventory
+  end
 end
